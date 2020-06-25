428e0e8362279d413125a46b8f715c8dcf2b6fe9
Aaron A. Kelley <aaron@aaron-kelley.net>
2020-06-25 14:01:49 -0400

    Shuffling code in DellFanLib.  DellFanLib-Public.h is removed.  DellFanLib.h now contains only the public interface for the DLL.  Everything else is in DellFanLib.cpp.

--- DellFanLib/DellFanLib.cpp
+++ DellFanLib/DellFanLib.cpp
@@ -1,6 +1,49 @@
-#include "DellFanLib.h"
+#include <windows.h>
 #include <winioctl.h>
+#include "DellFanLib.h"
 
+// Constants for controls.
+
+constexpr auto DELL_SMM_IO_DISABLE_FAN_CTL1 = 0x30A3;
+constexpr auto DELL_SMM_IO_ENABLE_FAN_CTL1 = 0x31A3;
+constexpr auto DELL_SMM_IO_DISABLE_FAN_CTL2 = 0x34A3;
+constexpr auto DELL_SMM_IO_ENABLE_FAN_CTL2 = 0x35A3;
+
+constexpr auto DELL_SMM_IO_SET_FAN_LV = 0x01A3;
+constexpr auto DELL_SMM_IO_GET_FAN_RPM = 0x02A3;
+
+constexpr auto DELL_SMM_IO_FAN_LV0 = 0;
+constexpr auto DELL_SMM_IO_FAN_LV1 = 1;
+constexpr auto DELL_SMM_IO_FAN_LV2 = 2;
+
+constexpr auto DELL_SMM_IO_FAN1 = 0;
+constexpr auto DELL_SMM_IO_FAN2 = 1;
+
+constexpr auto DELL_SMM_IO_NO_ARG = 0;
+
+// Other constants.
+
+constexpr auto FILE_DEVICE_BZH_DELL_SMM = 0xB424;
+constexpr auto BZH_DELL_SMM_IOCTL_KEY = 0xB42;
+#define IOCTL_BZH_DELL_SMM_RWREG CTL_CODE(FILE_DEVICE_BZH_DELL_SMM, BZH_DELL_SMM_IOCTL_KEY, METHOD_BUFFERED, FILE_ANY_ACCESS)
+
+// Structures.
+
+typedef struct {
+    unsigned long command;
+    unsigned long data;
+    unsigned long stat1;
+    unsigned long stat2;
+} SmmBiosPackage;
+
+// Private function headers.
+
+bool GetDriverPath();
+bool _stdcall InstallDriver(PWSTR pszDriverPath, bool IsDemandLoaded);
+bool _stdcall RemoveDriver();
+bool _stdcall StartDriver();
+bool _stdcall StopDriver();
+unsigned long _stdcall DellSmmIo(unsigned long command, unsigned long argument);
 
 // State.
 HANDLE driverHandle = INVALID_HANDLE_VALUE;
--- DellFanLib/DellFanLib.h
+++ DellFanLib/DellFanLib.h
@@ -1,52 +1,46 @@
 extern "C"
 {
-	#include <windows.h>
-
 	#define DllExport __declspec(dllexport)
 
-	// Constants for controls.
-
-	constexpr auto DELL_SMM_IO_DISABLE_FAN_CTL1 = 0x30A3;
-	constexpr auto DELL_SMM_IO_ENABLE_FAN_CTL1 = 0x31A3;
-	constexpr auto DELL_SMM_IO_DISABLE_FAN_CTL2 = 0x34A3;
-	constexpr auto DELL_SMM_IO_ENABLE_FAN_CTL2 = 0x35A3;
-
-	constexpr auto DELL_SMM_IO_SET_FAN_LV = 0x01A3;
-	constexpr auto DELL_SMM_IO_GET_FAN_RPM = 0x02A3;
-
-	constexpr auto DELL_SMM_IO_FAN_LV0 = 0;
-	constexpr auto DELL_SMM_IO_FAN_LV1 = 1;
-	constexpr auto DELL_SMM_IO_FAN_LV2 = 2;
-
-	constexpr auto DELL_SMM_IO_FAN1 = 0;
-	constexpr auto DELL_SMM_IO_FAN2 = 1;
-
-	constexpr auto DELL_SMM_IO_NO_ARG = 0;
-
-	// Other constants.
-
-	constexpr auto FILE_DEVICE_BZH_DELL_SMM = 0xB424;
-	constexpr auto BZH_DELL_SMM_IOCTL_KEY = 0xB42;
-	#define IOCTL_BZH_DELL_SMM_RWREG CTL_CODE(FILE_DEVICE_BZH_DELL_SMM, BZH_DELL_SMM_IOCTL_KEY, METHOD_BUFFERED, FILE_ANY_ACCESS)
-
-	// Structures.
-
-	typedef struct {
-		unsigned long command;
-		unsigned long data;
-		unsigned long stat1;
-		unsigned long stat2;
-	} SmmBiosPackage;
-
 	// Public function headers.
-	#include "DellFanLib-Public.h"
 
-	// Private function headers.
+	/// <summary>
+	/// Load the EC I/O driver and set things up.
+	/// </summary>
+	/// <returns>False if the driver failed to load</returns>
+	DllExport bool _stdcall Initialize();
 
-	bool GetDriverPath();
-	bool _stdcall InstallDriver(PWSTR pszDriverPath, bool IsDemandLoaded);
-	bool _stdcall RemoveDriver();
-	bool _stdcall StartDriver();
-	bool _stdcall StopDriver();
-	unsigned long _stdcall DellSmmIo(unsigned long command, unsigned long argument);
+	/// <summary>
+	/// Unload the EC I/O driver.
+	/// </summary>
+	DllExport void _stdcall Shutdown();
+
+	/// <summary>
+	/// Get the speed of a specific fan.
+	/// </summary>
+	/// <param name="fanIndex">Which fan to get the speed of (0 or 1)</param>
+	/// <returns>Speed in RPM; uint-max means failure</returns>
+	DllExport unsigned long GetFanRpm(unsigned long fanIndex);
+
+	/// <summary>
+	/// Disable EC fan control.
+	/// </summary>
+	/// <param name="alternate">If true, attempt alternate method to disable EC fan control</param>
+	/// <returns>ulong-max means failure</returns>
+	DllExport unsigned long DisableEcFanControl(bool alternate = false);
+
+	/// <summary>
+	/// Enable EC fan control.
+	/// </summary>
+	/// <param name="alternate">If ture, attempt alternate method to enable EC fan control</param>
+	/// <returns>ulong-max means failure</returns>
+	DllExport unsigned long EnableEcFanControl(bool alternate = false);
+
+	/// <summary>
+	/// Set the fan level of one of the fans.
+	/// </summary>
+	/// <param name="fanIndex">Which fan to set the level of (0 or 1)</param>
+	/// <param name="fanLevel">Which fan level to set (0 = off, 1 = medium, 2 = high)</param>
+	/// <returns>ulong-max means failure</returns>
+	DllExport unsigned long SetFanLevel(unsigned long fanIndex, unsigned long fanLevel);
 }
