aaa2ecb2586fee7e61133dc9ec2e65350dc913f1
Aaron A. Kelley <aaron@aaron-kelley.net>
2021-06-22 08:19:08 -0400

    Use DPI mode for scaling adjustment (...that will be the mode that I use *if* I flip on high-DPI support in the future).

--- DellFanManagementApp/DellFanManagementGuiForm.Designer.cs
+++ DellFanManagementApp/DellFanManagementGuiForm.Designer.cs
@@ -724,8 +724,8 @@ namespace DellFanManagement.App
             // 
             // DellFanManagementGuiForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
             this.ClientSize = new System.Drawing.Size(951, 315);
             this.Controls.Add(this.statusStrip);
             this.Controls.Add(this.audioKeepAliveGroupBox);
