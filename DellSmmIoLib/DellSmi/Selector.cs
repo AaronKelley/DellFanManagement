5f4226ecc3073e7840bd81f49f5488abf0d858ae
Aaron A. Kelley <aaron@aaron-kelley.net>
2021-07-23 09:39:25 -0400

    Comment sources for some of the selectors.

--- DellSmmIoLib/DellSmi/Selector.cs
+++ DellSmmIoLib/DellSmi/Selector.cs
@@ -5,27 +5,31 @@
         /// <summary>
         /// "Standard" tokens apply regardless of battery setting (?).
         /// </summary>
+        /// <seealso cref="https://github.com/torvalds/linux/blob/master/include/uapi/linux/wmi.h"/>
         Standard = 0,
 
         /// <summary>
         /// "Battery" token applies when the system is on battery power.
         /// </summary>
+        /// <seealso cref="https://github.com/torvalds/linux/blob/master/include/uapi/linux/wmi.h"/>
         Battery = 1,
 
         /// <summary>
         /// "AC" token applies when the system is on AC power.
         /// </summary>
+        /// <seealso cref="https://github.com/torvalds/linux/blob/master/include/uapi/linux/wmi.h"/>
         AC = 2,
 
         /// <summary>
         /// Used to get BIOS password information.
         /// </summary>
+        /// <seealso cref="https://github.com/dell/libsmbios/blob/master/src/libsmbios_c/smi/smi_password.c"/>
         PasswordProperties = 3,
 
         /// <summary>
         /// Used to enable or disable wireless connectivity.
         /// </summary>
-        /// /// <seealso cref="https://github.com/torvalds/linux/blob/master/include/uapi/linux/wmi.h"/>
+        /// <seealso cref="https://github.com/torvalds/linux/blob/master/include/uapi/linux/wmi.h"/>
         RfKill = 11,
 
         /// <summary>
